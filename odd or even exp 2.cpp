8f7bdbc744eacf16c6a79aceaf1b8d2410c9fcca
Yaswanthponnagantti <114177503+Yaswanthponnagantti@users.noreply.github.com>
2022-09-28 14:17:05 +0530

    Add files via upload

---
+++ odd or even exp 2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("enter a number");
+	scanf("%d",&num);
+	if(num%2==0)
+	printf("it is an even number");
+	else
+	printf("odd number");
+	return 0;
+}
